d2488cd0a76db3c305b79646d5cb7fc69d2c3a5c
Hempraw <78518989+Hempraw@users.noreply.github.com>
2021-03-08 14:18:08 +0900

    Rubocopの実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname                {Faker::Name.name}
-    email                   {Faker::Internet.free_email}
-    password                {"111aaa"}
-    password_confirmation   {password}
-    family_name             {"山田"}
-    first_name              {"太郎"}
-    family_name_kana        {"ヤマダ"}
-    first_name_kana         {"タロウ"}
-    birth_day               {Faker::Date.in_date_period}
+    nickname                { Faker::Name.name }
+    email                   { Faker::Internet.free_email }
+    password                { '111aaa' }
+    password_confirmation   { password }
+    family_name             { '山田' }
+    first_name              { '太郎' }
+    family_name_kana        { 'ヤマダ' }
+    first_name_kana         { 'タロウ' }
+    birth_day               { Faker::Date.in_date_period }
   end
 end
