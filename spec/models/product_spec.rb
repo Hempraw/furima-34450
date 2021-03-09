b0b81ee845b47597fe2f27dd9d0433b584c5e244
Hempraw <78518989+Hempraw@users.noreply.github.com>
2021-03-09 16:28:24 +0900

    プルリクエスト修正２
    rubocop実行

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -60,7 +60,7 @@ RSpec.describe Product, type: :model do
       it 'userが紐づいてなければ出品できない' do
         @product.user = nil
         @product.valid?
-        expect(@product.errors.full_messages).to include("User must exist")
+        expect(@product.errors.full_messages).to include('User must exist')
       end
       it 'priceが全角数字だと出品できない' do
         @product.price = '５００'
