b7be8ced52ad7bc764fd0cebb417d57a403cf60c
Hempraw <78518989+Hempraw@users.noreply.github.com>
2021-03-09 12:02:38 +0900

    バリデーションと出品のアカウント制限の実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,8 +8,9 @@ class Product < ApplicationRecord
     belongs_to :prefecture
     belongs_to :shipping_date
 
-    validates :category_id, :product_status_id, :delivery_fee_id, :prefecture_id, :shipping_date_id, numericality:{other_than: 1}
   with_options presence: true do
     validates :name, :description, :image
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
   end
+  validates :category_id, :product_status_id, :delivery_fee_id, :prefecture_id, :shipping_date_id, numericality:{other_than: 1, message: "select"}
 end
