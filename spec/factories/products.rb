007c39af71171c5bb8e98383d67b2f7559cbc0b2
Hempraw <78518989+Hempraw@users.noreply.github.com>
2021-03-09 14:14:05 +0900

    productモデル単体テスト

---
+++ spec/factories/products.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :product do
+    name                { Faker::Commerce.product_name }
+    description         { Faker::Lorem.paragraph_by_chars}
+    category_id         { Faker::Number.within(range: 2..11) }
+    product_status_id   { Faker::Number.within(range: 2..7) }
+    delivery_fee_id     { Faker::Number.within(range: 2..3) }
+    prefecture_id       { Faker::Number.within(range: 2..48) }
+    shipping_date_id    { Faker::Number.within(range: 2..4) }
+    price               { Faker::Number.within(range: 300..9999999)}
+    association :user
+
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/images/test_image.jpeg'), filename: 'test_image.jpeg')
+    end
+  end
+end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,87 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  before do
+    @product = FactoryBot.build(:product)
+  end
+
+  describe '商品出品機能' do
+    context '商品が出品できるとき' do
+      it '全ての項目が正しく入力されていれば出品ができる' do
+        expect(@product).to be_valid
+      end
+    end
+    context '商品が出品できないとき' do
+      it 'imageが空では出品できない' do
+        @product.image = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空では出品できない' do
+        @product.name = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'descriptionが空では出品できない' do
+        @product.description = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Description can't be blank") 
+      end
+      it 'categoryが未選択（---)では登録できない' do
+        @product.category_id = 1
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Category select")
+      end
+      it 'product_statusが未選択（---)では登録できない' do
+        @product.product_status_id = 1
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Product status select")
+      end
+      it 'delivery_feeが未選択（---)では登録できない' do
+        @product.delivery_fee_id = 1
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Delivery fee select")
+      end
+      it 'prefectureが未選択（---)では登録できない' do
+        @product.prefecture_id = 1
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Prefecture select")
+      end
+      it 'shipping_dateが未選択（---)では登録できない' do
+        @product.shipping_date_id = 1
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Shipping date select")
+      end
+      it 'priceが空では出品できない' do
+        @product.price = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが全角数字だと出品できない' do
+        @product.price = '５００'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price is invalid")
+      end
+      it 'priceが全角文字だと出品できない' do
+        @product.price = "五千"
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price is invalid")
+      end
+      it 'priceが半角文字だと出品できない' do
+        @product.price = 'five hungred'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price is invalid")
+      end
+      it 'priceが300以下だと出品できない' do
+        @product.price = 299
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price is invalid")
+      end
+      it 'priceが9999999以上だと出品できない' do
+        @product.price = 10000000
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price is invalid")
+      end
+    end
+  end
+end
