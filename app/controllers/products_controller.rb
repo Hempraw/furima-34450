6dedfb7b6cc418e9d4b84cddff940abfc880fa3e
Hempraw <78518989+Hempraw@users.noreply.github.com>
2021-03-10 14:27:38 +0900

    商品編集ページの実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :move_to_index, only: :edit
 
   def index
     @products = Product.all.order(created_at: :desc)
@@ -22,10 +23,31 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    product = Product.find(params[:id])
+    if product.update(product_params)
+      redirect_to product_path(product.id)
+    else
+      @product = Product.find(params[:id])
+      render :edit
+    end
+  end
+  
   private
 
   def product_params
     params.require(:product).permit(:image, :name, :description, :category_id, :product_status_id, :delivery_fee_id,
                                     :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+  
+  def move_to_index
+    @product = Product.find(params[:id])
+    unless current_user.id == @product.user_id
+      redirect_to action: :show
+    end
   end
 end
