435faa5527cbf810070e6c1ab36cd3512396d686
Hempraw <78518989+Hempraw@users.noreply.github.com>
2021-03-12 17:37:37 +0900

    クレジット決済の導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,8 +5,11 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @product = Product.find(params[:product_id])
     @order_address = OrderAddress.new(order_address_params)
+    binding.pry
     if @order_address.valid?
+      pay_product
       @order_address.save
       redirect_to root_path
     else
@@ -18,6 +21,15 @@ class OrdersController < ApplicationController
   private
   def order_address_params
     params.require(:order_address).permit(:postal_number, :prefecture_id, :city, :house_number,
-                                          :building_name, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id])
+                                          :building_name, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id], token: params[:token])
+  end
+
+  def pay_product
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @product.price,
+      card: order_address_params[:token],
+      currency:'jpy'
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,11 +1,11 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :product_id, :postal_number, :prefecture_id, :city, :house_number,
-                :building_name, :phone_number, :order_id
+                :building_name, :phone_number, :order_id, :token
 
   with_options presence: true do
     validates :postal_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :city, :house_number, :user_id, :product_id
+    validates :city, :house_number, :user_id, :product_id, :token
     validates :phone_number, format: {with: /\A\d{11}\z/, message: "is invalid"}
   end
   validates :prefecture_id, numericality: { other_than: 1, message: 'select' }
